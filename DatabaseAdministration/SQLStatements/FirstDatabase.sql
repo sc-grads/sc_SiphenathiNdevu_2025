d82a241582c9ecccf3ed85b4311a7190680a9a07
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-03-11 10:32:39 +0200

    Added new file for firstDatabase Example

---
+++ DatabaseAdministration/SQLStatements/FirstDatabase.sql
@@ -0,0 +1,16 @@
+USE [FirstDatabase]
+GO
+
+INSERT INTO [dbo].[personalInfo]
+           ([firstName]
+           ,[lastName]
+           ,[dob]
+           ,[ID])
+     VALUES
+           ('Abbas'
+           ,'Haba'
+           ,'02/05/2024'
+           ,777)
+GO
+
+
