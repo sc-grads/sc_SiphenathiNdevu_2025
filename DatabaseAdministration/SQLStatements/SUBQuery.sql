8139aae7e328027e6b807063991bbb7566a596f7
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-03-12 08:37:27 +0200

    Sub query scripts

---
+++ DatabaseAdministration/SQLStatements/SUBQuery.sql
@@ -0,0 +1,17 @@
+SELECT * FROM [HumanResources].[EmployeePayHistory]
+WHERE BusinessEntityID IN (SELECT BusinessEntityID FROM [HumanResources].[EmployeePayHistory] WHERE Rate > 60)
+
+
+
+SELECT * FROM [Employee]
+
+----------------------------------------
+
+SELECT *
+  FROM [HumanResources].[EmployeePayHistory]
+  WHERE [BusinessEntityID] IN (SELECT BusinessEntityID FROM [HumanResources].[EmployeePayHistory] WHERE Rate = 39.06)
+
+  ---------------------------------------------
+
+ select * from [Production].[Product]
+ where ProductID in ( select ProductID from [Production].[ProductInventory] where Quantity > 300)
