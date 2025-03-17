872391d98efa85c2945f2137d342f7379f3b2932
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-03-17 14:34:26 +0200

    Section 9 - MSSQL Backup and Restore

---
+++ DatabaseAdministration/SQLStatements/TransactionLogBackup.sql
@@ -0,0 +1,2 @@
+BACKUP LOG [AdventureWorks2022] TO  DISK = N'C:\Backup\AdventureWorks2022.bak' WITH NOFORMAT, NOINIT,  NAME = N'AdventureWorks2022-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
