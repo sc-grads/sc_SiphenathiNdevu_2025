58801df328156f3e6d8f9189171812fae23eff91
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-04-01 10:48:23 +0200

    Linux commands

---
+++ Linux/commands.sh
@@ -0,0 +1,10 @@
+/ #“Root,” the top of the file system hierarchy.
+/etc #System configuration files.
+/home/user #Prints the current working directory
+echo "Hello, Linux!" #echo Displays text
+/bin #Binaries and other executable programs
+/opt #Optional or third party software
+/tmp #Temporary space, typically cleared on reboot
+/usr #User related programs.
+/var #Variable data, most notably log files.
+ 
