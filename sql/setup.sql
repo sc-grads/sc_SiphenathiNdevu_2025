dc47327e9f4f4f7619c9fc92b7327540e4598eaf
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-04-02 23:16:25 +0200

    Update setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,57 +1,103 @@
 -- Step 1: Create AutoTestSiphenathi database
-USE master;
-GO
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AutoTestSiphenathi')
-BEGIN
-    CREATE DATABASE AutoTestSiphenathi;
-    PRINT 'Database AutoTestSiphenathi created.';
-END
-ELSE
-    PRINT 'Database AutoTestSiphenathi already exists.';
+BEGIN TRY
+    USE master;
+    IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AutoTestSiphenathi')
+    BEGIN
+        CREATE DATABASE AutoTestSiphenathi;
+        PRINT 'Database AutoTestSiphenathi created.';
+    END
+    ELSE
+        PRINT 'Database AutoTestSiphenathi already exists.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error creating database AutoTestSiphenathi!';
+    PRINT ERROR_MESSAGE();
+END CATCH;
 GO
 
 -- Step 2: Switch to AutoTestSiphenathi
-USE AutoTestSiphenathi;
+BEGIN TRY
+    USE AutoTestSiphenathi;
+    PRINT 'Switched to database AutoTestSiphenathi.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error switching to AutoTestSiphenathi!';
+    PRINT ERROR_MESSAGE();
+END CATCH;
 GO
 
 -- Step 3: Create user table
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user')
-BEGIN
-    CREATE TABLE [user] (
-        Name VARCHAR(50),
-        Surname VARCHAR(50),
-        Email VARCHAR(100)
-    );
-    PRINT 'Table [user] created.';
-END
-ELSE
-    PRINT 'Table [user] already exists.';
+BEGIN TRY
+    IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user')
+    BEGIN
+        CREATE TABLE [user] (
+            Name VARCHAR(50),
+            Surname VARCHAR(50),
+            Email VARCHAR(100)
+        );
+        PRINT 'Table [user] created.';
+    END
+    ELSE
+        PRINT 'Table [user] already exists.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error creating table [user]!';
+    PRINT ERROR_MESSAGE();
+END CATCH;
 GO
 
 -- Step 4: Create InsertUser stored procedure
-IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'InsertUser')
-BEGIN
-    DROP PROCEDURE InsertUser;
-END
-GO
-CREATE PROCEDURE InsertUser
-    @Name VARCHAR(50),
-    @Surname VARCHAR(50),
-    @Email VARCHAR(100)
-AS
-BEGIN
-    INSERT INTO [user] (Name, Surname, Email)
-    VALUES (@Name, @Surname, @Email);
-    PRINT 'Inserted: ' + @Name + ' ' + @Surname + ', ' + @Email;
-END;
+BEGIN TRY
+    IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'InsertUser')
+    BEGIN
+        DROP PROCEDURE InsertUser;
+    END;
+    
+    EXEC('
+    CREATE PROCEDURE InsertUser
+        @Name VARCHAR(50),
+        @Surname VARCHAR(50),
+        @Email VARCHAR(100)
+    AS
+    BEGIN
+        BEGIN TRY
+            INSERT INTO [user] (Name, Surname, Email)
+            VALUES (@Name, @Surname, @Email);
+            PRINT ''Inserted: '' + @Name + '' '' + @Surname + '', '' + @Email;
+        END TRY
+        BEGIN CATCH
+            PRINT ''Error inserting user: '' + ERROR_MESSAGE();
+        END CATCH
+    END;
+    ');
+
+    PRINT 'Stored procedure InsertUser created.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error creating stored procedure InsertUser!';
+    PRINT ERROR_MESSAGE();
+END CATCH;
 GO
 
 -- Step 5: Insert initial data
-EXEC InsertUser @Name = 'Siphenathi', @Surname = 'Ndevu', @Email = 'siphenathi@example.com';
-EXEC InsertUser @Name = 'Partner', @Surname = 'One', @Email = 'partner@example.com';
+BEGIN TRY
+    EXEC InsertUser @Name = 'Siphenathi', @Surname = 'Ndevu', @Email = 'siphenathi@example.com';
+    EXEC InsertUser @Name = 'Partner', @Surname = 'One', @Email = 'partner@example.com';
+    PRINT 'Initial data inserted.';
+END TRY
+BEGIN CATCH
+    PRINT 'Error inserting initial data!';
+    PRINT ERROR_MESSAGE();
+END CATCH;
 GO
 
 -- Step 6: Verify
-SELECT 'Verification: user table contents' AS Message;
-SELECT * FROM [user];
+BEGIN TRY
+    PRINT 'Verification: Checking user table contents...';
+    SELECT * FROM [user];
+END TRY
+BEGIN CATCH
+    PRINT 'Error verifying user table!';
+    PRINT ERROR_MESSAGE();
+END CATCH;
 GO
