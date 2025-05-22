8bcab795f789be170d5cd259e7910eff09caa00e
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-05-22 14:53:03 +0200

    Sub-queries

---
+++ DatabaseAdministration/T-SQL/SelectClause.sql
@@ -0,0 +1,20 @@
+Select *, (select count(EmployeeNumber)
+           from tblTransaction as T
+		   where T.EmployeeNumber = E.EmployeeNumber) as NumTransactions,
+		  (Select sum(Amount)
+		   from tblTransaction as T
+		   where T.EmployeeNumber = E.EmployeeNumber) as TotalAmount
+from tblEmployee as E
+Where E.EmployeeLastName like 'y%' --correlated subquery
+Remainder
+select * 
+from tblTransaction as T
+Where exists 
+    (Select EmployeeNumber from tblEmployee as E where EmployeeLastName like 'y%' and T.EmployeeNumber = E.EmployeeNumber)
+order by EmployeeNumber
+
+select * 
+from tblTransaction as T
+Where not exists 
+    (Select EmployeeNumber from tblEmployee as E where EmployeeLastName like 'y%' and T.EmployeeNumber = E.EmployeeNumber)
+order by EmployeeNumber
