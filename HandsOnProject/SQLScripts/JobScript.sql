b52a94bd286de1ca01637fd2b1a713ebe75b8f29
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting.com>
2025-06-24 20:04:17 +0200

    Update JobScript.sql

--- HandsOnProject/SQLScripts/JobScript.sql
+++ HandsOnProject/SQLScripts/JobScript.sql
@@ -1,40 +1,51 @@
-USE msdb;
+USE [msdb];
 GO
 
--- Delete existing job if it exists
-IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'@jobName')
-    EXEC msdb.dbo.sp_delete_job @job_name = N'@jobName';
+-- Remove existing job
+IF EXISTS (SELECT job_id FROM msdb.dbo.sysjobs WHERE name = N'{jobName}')
+BEGIN
+    EXEC msdb.dbo.sp_delete_job @job_name = N'{jobName}';
+END
 GO
 
--- Create new job
-EXEC msdb.dbo.sp_add_job
-    @job_name = N'@jobName',
+-- Create job
+DECLARE @jobId BINARY(16);
+EXEC msdb.dbo.sp_add_job 
+    @job_name = N'{jobName}',
     @enabled = 1,
-    @description = N'Job to run SSIS package @jobName',
-    @owner_login_name = N'sa';
+    @notify_level_eventlog = 2,
+    @delete_level = 0,
+    @description = N'Scheduled job to execute SSIS package: {jobName} every 1 minute (30s not supported)',
+    @category_name = N'[Uncategorized (Local)]',
+    @owner_login_name = N'{dbUser}', -- To be replaced with actual username
+    @job_id = @jobId OUTPUT;
 GO
 
--- Add job step to execute SSIS package
-EXEC msdb.dbo.sp_add_jobstep
-    @job_name = N'@jobName',
-    @step_name = N'Execute SSIS Package',
+-- Add job step to run SSIS package
+EXEC msdb.dbo.sp_add_jobstep 
+    @job_name = N'{jobName}',
+    @step_name = N'Run SSIS Package',
     @subsystem = N'SSIS',
-    @command = N'/ISSERVER "\SSISDB\TimesheetDeployedPackages\@jobName\Package.dtsx" /SERVER "LAPTOP-ATT0UPK9"',
-    @database_name = N'master';
+    @command = N'/ISSERVER "\"\SSISDB\TimesheetDeployedPackages\ProjectPackages\{jobName}\"" /SERVER "{dbServer}"',
+    @database_name = N'master',
+    @on_success_action = 1,
+    @on_fail_action = 2;
 GO
 
--- Add job schedule (e.g., daily at 2 AM)
-EXEC msdb.dbo.sp_add_jobschedule
-    @job_name = N'@jobName',
-    @name = N'DailySchedule',
+-- Single schedule: starts at 00:00:00, every 1 minute
+EXEC msdb.dbo.sp_add_jobschedule 
+    @job_name = N'{jobName}',
+    @name = N'RunEveryMinute',
     @enabled = 1,
-    @freq_type = 4, -- Daily
+    @freq_type = 4,  -- daily
     @freq_interval = 1,
-    @active_start_time = 020000; -- 2:00 AM
+    @freq_subday_type = 4, -- minutes
+    @freq_subday_interval = 1, -- every 1 minute
+    @active_start_time = 000000;
 GO
 
--- Add job to server
-EXEC msdb.dbo.sp_add_jobserver
-    @job_name = N'@jobName',
-    @server_name = N'(local)';
+-- Attach job to current server
+EXEC msdb.dbo.sp_add_jobserver 
+    @job_name = N'{jobName}',
+    @server_name = N'(LOCAL)'; -- Use (LOCAL) for the local instance
 GO
