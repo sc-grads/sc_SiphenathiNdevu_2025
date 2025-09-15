45b5f04c6cea0a2eb46e896279cab0707f3c80bd
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-09-15 16:43:14 +0200

    Project

---
+++ PostgreSQL/ig/migrations/1757506712336_add-posts-table.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql(`
+        CREATE TABLE posts (
+            id SERIAL PRIMARY KEY,
+            url VARCHAR(300),
+            lat NUMERIC,
+            lng NUMERIC
+        );
+    `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql(`
+        DROP TABLE posts;
+    `);
+};
