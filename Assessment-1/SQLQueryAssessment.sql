74d88da12f4830d853179550b788f1696d404222
NdevuSiphenathi <siphenathi.ndevu@sambeconsulting>
2025-12-11 10:02:56 +0200

    Assessment2

--- Assessment-1/SQLQueryAssessment.sql
+++ Assessment-1/SQLQueryAssessment.sql
@@ -29,7 +29,6 @@ ORDER BY subtotal DESC
 SELECT Distinct JobTitle
 FROM [HumanResources].[Employee]
 ORDER BY [JobTitle] ASC;
-
 --------------------------------------7
 
 SELECT  customerid, SUM(Freight) AS total_freight
@@ -91,7 +90,8 @@ GROUP BY ROLLUP (locationid)
 ORDER BY locationid;
 
 -----------------------------------------16
-SELECT city,COUNT(*) AS noofemployees FROM [Person].[Address]
+SELECT city,COUNT(*) AS noofemployees 
+FROM [Person].[Address]
 GROUP BY city
 ORDER BY city Asc
 -----------------------------------------17
@@ -121,37 +121,61 @@ INNER JOIN [Person].[BusinessEntityContact] as pb ON p.[BusinessEntityID] = pb.[
 INNER JOIN [Person].[ContactType] as pc ON pb.[ContactTypeID] = pc.[ContactTypeID]
 WHERE pc.Name = 'Purchasing Manager'
 -----------------------------------------21
-
-
+SELECT ROW_NUMBER() OVER (
+        PARTITION BY a.PostalCode 
+        ORDER BY sp.SalesYTD DESC
+    ) AS RowNum,
+    p.LastName,
+    sp.SalesYTD,
+    a.PostalCode
+FROM Sales.SalesPerson sp
+INNER JOIN Person.Person p 
+    ON sp.BusinessEntityID = p.BusinessEntityID
+INNER JOIN Person.Address a
+    ON p.BusinessEntityID = a.AddressID
+INNER JOIN Sales.SalesTerritory st
+    ON sp.TerritoryID = st.TerritoryID
+WHERE 
+    sp.SalesYTD <> 0
+    AND a.PostalCode IS NOT NULL
+ORDER BY 
+    a.PostalCode ASC,
+    sp.SalesYTD DESC;
 
 -----------------------------------------22
-SELECT ct.ContactTypeID, ct.Name AS ContactTypeName, COUNT(*) AS NoContacts FROM Person.ContactType AS ct
+SELECT ct.ContactTypeID, ct.Name AS ContactTypeName, COUNT(*) AS NoContacts 
+FROM Person.ContactType AS ct
 JOIN Person.BusinessEntityContact AS bec ON ct.ContactTypeID = bec.ContactTypeID
 GROUP BY ct.ContactTypeID, ct.Name HAVING COUNT(*) >= 100
 ORDER BY NoContacts DESC
 
 -----------------------------------------23
 SELECT CONVERT(date, eph.RateChangeDate) AS fromdate, p.LastName + ', ' + p.FirstName + COALESCE(' ' + LEFT(p.MiddleName, 1), '') AS nameinfull,
-CAST(eph.Rate * 40 AS decimal(18,4)) AS salaryinaweek FROM HumanResources.EmployeePayHistory AS eph
+CAST(eph.Rate * 40 AS decimal(18,4)) AS salaryinaweek 
+FROM HumanResources.EmployeePayHistory AS eph
 INNER JOIN HumanResources.Employee AS e ON e.BusinessEntityID = eph.BusinessEntityID
 INNER JOIN Person.Person AS p ON p.BusinessEntityID = e.BusinessEntityID
 ORDER BY nameinfull ASC
 
 -----------------------------------------24
 SELECT CONVERT(date, eph.RateChangeDate) AS fromdate, p.LastName + ', ' + p.FirstName + COALESCE(' ' + LEFT(p.MiddleName, 1), '') AS nameinfull,
-CAST(eph.Rate * 40 AS decimal(18,4)) AS salaryinaweek FROM HumanResources.EmployeePayHistory AS eph
+CAST(eph.Rate * 40 AS decimal(18,4)) AS salaryinaweek 
+FROM HumanResources.EmployeePayHistory AS eph
 INNER JOIN HumanResources.Employee AS e ON e.BusinessEntityID = eph.BusinessEntityID
 INNER JOIN Person.Person AS p ON p.BusinessEntityID = e.BusinessEntityID
-INNER JOIN (SELECT BusinessEntityID, MAX(RateChangeDate) AS LatestChange FROM HumanResources.EmployeePayHistory
+INNER JOIN (SELECT BusinessEntityID, MAX(RateChangeDate) AS LatestChange 
+FROM HumanResources.EmployeePayHistory
 GROUP BY BusinessEntityID
 ) AS latest ON latest.BusinessEntityID = eph.BusinessEntityID AND latest.LatestChange = eph.RateChangeDate
 ORDER BY nameinfull ASC
 
 -----------------------------------------25
 SELECT d.SalesOrderID, d.ProductID, d.OrderQty, agg.TotalQty AS [Total Quantity], agg.AvgQty AS [Avg Quantity], agg.OrderCount AS [No of Orders],
-agg.MinQty AS [Min Quantity], agg.MaxQty AS [Max Quantity] FROM Sales.SalesOrderDetail AS d
+agg.MinQty AS [Min Quantity], agg.MaxQty AS [Max Quantity] 
+FROM Sales.SalesOrderDetail AS d
 CROSS JOIN (SELECT SUM(d2.OrderQty) AS TotalQty, AVG(CAST(d2.OrderQty AS decimal(18,6))) AS AvgQty, COUNT(*) AS OrderCount, MIN(d2.OrderQty) AS MinQty,
-MAX(d2.OrderQty) AS MaxQty FROM Sales.SalesOrderDetail AS d2
+MAX(d2.OrderQty) AS MaxQty 
+FROM Sales.SalesOrderDetail AS d2
 WHERE d2.SalesOrderID IN (43659, 43664)) AS agg
 WHERE d.SalesOrderID IN (43659, 43664)
 ORDER BY d.SalesOrderID, d.ProductID
